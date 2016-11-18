64740fe84242aaa4c3cb23fb814a826f40770ff6
Chris Freeman <cfreeman.code@gmail.com>
2016-11-18 14:50:34 -0600

    Initial commit
    Alllllll the config files

---
+++ libs/parts.js
@@ -0,0 +1,120 @@
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack-plugin');
+
+exports.setFreeVariable = function(key, value) {
+  const env = {};
+  env[key] = JSON.stringify(value);
+
+  return {
+    plugins: [
+      new webpack.DefinePlugin(env)
+    ]
+  };
+}
+
+exports.typescript = function(paths) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          include: paths,
+          loader: 'awesome-typescript-loader'
+        }
+      ]
+    }
+  };
+}
+
+exports.devServer = function(options) {
+  return {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+
+      stats: 'errors-only',
+
+      host: options.host,
+      port: options.port
+    },
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin({
+        multiStep:true
+      })
+    ]
+  };
+}
+
+exports.css = function(paths) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader'],
+          include: paths
+        }
+      ]
+    }
+  };
+}
+
+exports.minify = function() {
+  return {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        mangle: {
+          screw_ie8: true
+        }
+      }),
+    ],
+  };
+}
+
+exports.clean = function(path) {
+  return {
+    plugins: [
+      new CleanWebpackPlugin([path], {
+        root: process.cwd()
+      })
+    ]
+  };
+}
+
+exports.extractCSS = function(paths) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include: paths,
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: 'style-loader',
+            loader: 'css-loader'
+          })
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('[name].css')
+    ]
+  };
+}
+
+exports.purifyCSS = function(paths) {
+  return {
+    plugins: [
+      new PurifyCSSPlugin({
+        basePath: process.cwd(),
+        paths: paths
+      })
+    ]
+  };
+}
---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+const parts = require('./libs/parts')
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  style: [
+    path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css', 'bootstrap.css'),
+    path.join(__dirname, 'src', 'styles.css')
+  ],
+  dist: path.join(__dirname, 'dist')
+};
+
+const common = {
+  entry: {
+    src: PATHS.src,
+    style: PATHS.style
+  },
+
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+  },
+
+  output: {
+    path: PATHS.dist,
+    filename: '[name].js',
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'mobx-dbmon'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'file-loader?name=[path][name].[hash].[ext]',
+      },
+      {
+        'test': /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?/,
+        loader: 'url-loader',
+        query: {
+          prefix: 'font/',
+          limit: 10000,
+          mimetype: 'application/font-woff',
+        },
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?/,
+        loader: 'url-loader',
+      },
+    ]
+  }
+}
+
+switch (process.env.npm_lifecycle_event) {
+  // production build
+  case 'compile':
+    config = merge(
+      common,
+      {
+        devtool: 'source-map'
+      },
+      parts.clean(PATHS.dist),
+      parts.setFreeVariable(
+        'process.env.NODE_ENV',
+        'production'
+      ),
+      parts.typescript(PATHS.src),
+      parts.minify(),
+      parts.extractCSS(PATHS.style),
+      parts.purifyCSS([PATHS.src])
+    );
+    break;
+  // dev build
+  default:
+    config = merge(
+      common,
+      {
+        devtool: 'eval-source-map'
+      },
+      parts.css(PATHS.style),
+      parts.typescript(PATHS.src),
+      parts.devServer({
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
+}
+
+module.exports = config;
